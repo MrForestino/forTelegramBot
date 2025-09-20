34e1eeaa098a6102b921ab1b724f822ac76dd80a
MrForestino <borv2006@gmail.com>
2025-09-20 18:55:16 +0200

    update

--- tgBot.js
+++ tgBot.js
@@ -1,18 +1,23 @@
 import TelegramBot from "node-telegram-bot-api";
 
-const TOKEN = "8434812242:AAHMguh1lLiH2kiuARLUKwTrCdM__oGlDfM";
+const TOKEN = "тут_твій_API_TOKEN";
 const CHANNEL_ID = "@tgchanel_fortest"; // username або id каналу
 const bot = new TelegramBot(TOKEN, { polling: false });
 
-bot.sendMessage(CHANNEL_ID, "🎉 Розіграш! Натисни кнопку, щоб взяти участь:", {
-  reply_markup: {
-    inline_keyboard: [
-      [
-        {
-          text: "Взяти участь ✅",
-          web_app: { url: "https://mrforestino.github.io/forTelegramBot/" } // твій сайт/MiniApp
-        }
+// Надсилаємо повідомлення у канал
+bot.sendMessage(
+  CHANNEL_ID,
+  "🎉 Розіграш! Натисни кнопку, щоб взяти участь:",
+  {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "Взяти участь ✅",
+            url: "https://mrforestino.github.io/forTelegramBot/" // твій сайт/MiniApp
+          }
+        ]
       ]
-    ]
+    }
   }
-});
+);
